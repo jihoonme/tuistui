6accdceb80cc3c18eb26daa97fe89d79a76602a6
jihoonahn <jihoonahn.dev@gmail.com>
2025-05-13 01:23:14 +0900

    Update Package platform version

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "tuistui",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v14)],
     products: [
         .executable(
             name: "tuist-ui",
