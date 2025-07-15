caf6aa8255a5a4a92eeaad9854869dd17603a09b
jihoonahn <jihoonahn.dev@gmail.com>
2025-07-16 01:25:33 +0900

    Update XCConfigTarget path method

--- ProjectDescriptionHelpers/Component/XCConfig/XCConfigTargetType.swift
+++ ProjectDescriptionHelpers/Component/XCConfig/XCConfigTargetType.swift
@@ -3,5 +3,5 @@ import ProjectDescription
 /// A Protocol for management configurations in one place
 public protocol XCConfigTargetType: Hashable, CaseIterable, RawRepresentable where RawValue == String {
     /// Specify the configuration path.
-    var path: ProjectDescription.Path { get }
+    func path(for config: ConfigurationName) -> Path
 }
