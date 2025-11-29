21a5e104223997aefc7c1b8058a62b4329afd2f9
jihoonahn <jihoonahn.dev@gmail.com>
2025-11-30 01:27:43 +0900

    Update Swift Version 6.0 -> 6.2

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.3"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.5"),
         .package(url: "https://github.com/tuist/tuist", from: "4.109.1"),
     ],
     targets: [
